c980d5a8fe1c6623291aead22fdb53b1c6d656b9
sayedpalestine7 <sayed-zx7@hotmail.com>
2025-10-04 13:34:20 +0300

    Chapter 3 is completed.

--- chapter_3/src/middleware/authMiddleware.js
+++ chapter_3/src/middleware/authMiddleware.js
@@ -1,19 +1,16 @@
-import jwt, { decode } from "jsonwebtoken";
+import jwt from 'jsonwebtoken'
 
 function authMiddleware(req, res, next) {
-  const token = req.headers["authorization"];
-  if (!token) {
-    res.status(401).json({ message: "No token provided" });
-  }
+    const token = req.headers['authorization']
 
-  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-    if (error) {
-      return res.status(401).json({ message: "Failed to authenticate token" });
-    }
+    if (!token) { return res.status(401).json({ message: "No token provided" }) }
 
-    req.userId = decoded.id; //what is the decoded.id ? it is the id we set when we created the token in authRoutes.js
-    next();
-  });
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) { return res.status(401).json({ message: "Invalid token" }) }
+
+        req.userId = decoded.id
+        next()
+    })
 }
 
-export default authMiddleware;
+export default authMiddleware
--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -12,17 +12,32 @@ router.get("/", (req, res) => {
 
 // Create a new todo
 router.post("/", (req, res) => {
-  res.send("Create a new todo");
+  const { task } = req.body;
+  const insertTodo = db.prepare("INSERT INTO todos (user_id, task) VALUES (?, ?)");
+  const result = insertTodo.run(req.userId, task);
+
+  res.json({ id: result.lastInsertRowid, task, completed: 0 });
 });
 
 // Update a todo
 router.put("/:id", (req, res) => {
-  res.send("Update a todo");
+  const { completed } = req.body;
+  const { id } = req.params;
+  // const { page } = req.query;
+
+  const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`);
+  updatedTodo.run(completed, id);
+
+  res.json({ message: "Todo updated successfully" });
 });
 
 // Delete a todo
 router.delete("/:id", (req, res) => {
-  res.send("Delete a todo");
+  const { id } = req.params;
+  const userId = req.userId;
+  const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+  deleteTodo.run(id, userId);
+  res.json({ message: "Todo deleted successfully" });
 });
 
 export default router;
