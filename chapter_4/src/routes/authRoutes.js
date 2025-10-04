f9600325a292f5e8aed1fe8bbf38be6d933705ff
sayedpalestine7 <sayed-zx7@hotmail.com>
2025-10-04 14:47:57 +0300

    init project + added prisma schema and some changes

---
+++ chapter_4/src/routes/authRoutes.js
@@ -0,0 +1,99 @@
+import experss from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import db from "../db.js";
+import prisma from "../../prismaClient.js";
+
+const router = experss.Router();
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Encrypt the password before storing it
+  const hashedPassword = bcrypt.hashSync(password, 8);
+  // save the new user and hased password to the database
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      }
+    })
+
+    // now that we have a user i want to create there first todo for them
+    const defaultTodo = `Hello :) Add your fisrt todo!`;
+    await prisma.todo.create({
+      data: {
+        text: defaultTodo,
+        userId: user.id,
+      }
+    })
+
+    // Create a token
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(503);
+  }
+  // Removed duplicate response to prevent ERR_HTTP_HEADERS_SENT
+});
+
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username
+      }
+    });
+
+    if(!user){
+        return res.status(404).send({message: "User not found"});
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password); // returns true or false
+    if(!passwordIsValid){
+        return res.status(401).send({message: "Invalid password"});
+    }
+    console.log("User logged in:", user);
+
+    const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: "24h"});
+    res.json({token});
+
+  } catch (error) {
+    cosnole.log(error.message);
+    res.sendStatus(503);
+  }
+});
+
+// router.post("/register", (req, res) => {
+//     const {username, password} = req.body;
+//     if(!username || !password) {
+//         return res.status(400).json({message: "Username and password are required"});
+//     }
+//     const hashedPassword = bcrypt.hashSync(password, 8);
+//     try {
+//         const stmt = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+//         const info = stmt.run(username, hashedPassword);
+//         const user = {id: info.lastInsertRowid, username};
+//         const token = jwt.sign(user
+//         , "secretkey", {expiresIn: "1h"});
+//         res.status(201).json({user, token});
+//     } catch (err) {
+//         if (err.code === "SQLITE_CONSTRAINT_UNIQUE") {
+//             return res.status(409).json({message: "Username already exists"});
+//         }
+//         res.status(500).json({message: "Internal server error"});
+//     }
+// });
+
+export default router;
---
+++ chapter_4/src/routes/todoRoutes.js
@@ -0,0 +1,63 @@
+import express from "express";
+import db from "../db.js";
+import prisma from "../../prismaClient.js";
+
+const router = express.Router();
+
+//Get all todos for a user
+router.get("/", async (req, res) => {
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: req.userId
+    }
+  })
+  res.json(todos);
+});
+
+// Create a new todo
+router.post("/", async (req, res) => {
+  const { task } = req.body;
+  const todo = await prisma.todo.create({
+    data: {
+      task,
+      userId: req.userId
+    }
+  })
+
+  res.json(todo);
+});
+
+// Update a todo
+router.put("/:id", async (req, res) => {
+  const { completed } = req.body;
+  const { id } = req.params;
+  // const { page } = req.query;
+
+  const updatedTodo = await prisma.todo.update({
+    where: {
+      id: parseInt(id),
+      userId: req.userId
+    },
+    data: {
+      completed: !!completed
+    }
+  })
+
+  res.json(updatedTodo);
+});
+
+// Delete a todo
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const userId = req.userId;
+
+  const todo = await prisma.todo.delete({
+    where: {
+      id: parseInt(id),
+      userId: userId
+    }
+  })
+  res.json({ message: "Todo deleted successfully" });
+});
+
+export default router;
